724f356303a7017a97fe97b474ea0391ff269162
Ravi Karmakar <ravikarmkar94475@gmail.com>
2024-11-06 02:55:00 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+// import Button from "./components/Button.jsx";
+import Header from "./components/Header.jsx";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
